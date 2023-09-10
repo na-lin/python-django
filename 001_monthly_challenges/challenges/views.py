22e5ac511d622649f3b385ddacc54416b42c2af1
Na Lin <lna483018@gmail.com>
2023-09-10 11:34:26 -0400

    add more view and url

--- 001_monthly_challenges/challenges/views.py
+++ 001_monthly_challenges/challenges/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):
-  return HttpResponse("This works")
 
+def index(request):
+    return HttpResponse("This works")
+
+
+def feburary(request):
+    return HttpResponse("Work for at least 20 minutes every day!")
